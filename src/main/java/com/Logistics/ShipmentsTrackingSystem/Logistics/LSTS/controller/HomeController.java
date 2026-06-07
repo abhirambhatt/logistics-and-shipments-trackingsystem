9d5f7f3de4367d8364e3c914d0bf89185dbaf71e
Abhiram <abhiram.b@icloud.com>
2026-06-07 17:59:05 +0530

    Update HomeController.java

--- src/main/java/com/Logistics/ShipmentsTrackingSystem/Logistics/LSTS/controller/HomeController.java
+++ src/main/java/com/Logistics/ShipmentsTrackingSystem/Logistics/LSTS/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.Logistics.ShipmentsTrackingSystem.Logistics.LSTS.controller;
 
-import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
