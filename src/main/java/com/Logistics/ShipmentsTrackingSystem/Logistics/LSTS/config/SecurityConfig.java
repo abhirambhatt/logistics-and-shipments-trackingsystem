8d2da4d190baf0855e0d1d8b657cdf936648f2d2
Abhiram <abhiram.b@icloud.com>
2026-06-07 17:49:06 +0530

    Update SecurityConfig.java

--- src/main/java/com/Logistics/ShipmentsTrackingSystem/Logistics/LSTS/config/SecurityConfig.java
+++ src/main/java/com/Logistics/ShipmentsTrackingSystem/Logistics/LSTS/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/**").hasRole("ADMIN")
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/").permitAll()
                         .requestMatchers("/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
